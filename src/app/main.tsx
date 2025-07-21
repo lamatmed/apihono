55cb7a30d7fa82c77d2e9336104c9e1aa2a1bd3d
Lamat Abdellahi <lamat032025@gmail.com>
2025-07-21 12:05:39 +0100

    init

--- src/app/main.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import HomePage from "./index";
-
-const root = createRoot(document.getElementById("root")!);
-root.render(<HomePage />); 
